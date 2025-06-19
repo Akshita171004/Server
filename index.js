1c45b0694cd7324a74d192cd6e554696d5e2b0cb
Akshita171004 <akshitakaushik17@gmail.com>
2025-06-19 09:22:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => 
+{
+  res.send("Hello, World!");
+});
+app.post("/register", (req, res) =>
+    {
+        try {
+            const usersDir = path.join(__dirname, 'users');
+            
+            const files = fs.readdirSync(usersDir);
+            
+            const userFiles = files.filter(file => /^user\d+\.json$/.test(file));
+            const userNumber = userFiles.length + 1;
+            const filename = `user${userNumber}.txt`;
+            const filepath = path.join(usersDir, filename);
+
+            fs.writeFileSync(filepath, JSON.stringify(req.body, null, 2));
+            res.json({ message: `User saved as ${filename}` });
+          } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+          }
+    });
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
